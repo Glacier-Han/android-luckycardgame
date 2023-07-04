1346e5ab1977fc35015d214433f51841c042c6a2
Glacier Han <61905052+Glacier-Han@users.noreply.github.com>
2023-07-04 18:09:17 +0900

    FEAT: SegmentedButton 추가 및 기능구현

--- app/src/main/java/com/glacier/luckycardgamesofteer/MainActivity.kt
+++ app/src/main/java/com/glacier/luckycardgamesofteer/MainActivity.kt
@@ -3,14 +3,56 @@ package com.glacier.luckycardgamesofteer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.appcompat.content.res.AppCompatResources
+import com.glacier.luckycardgamesofteer.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
+        init()
         pickCard(initCard())
+    }
+
+    fun init(){
+        binding.mbToggle.addOnButtonCheckedListener { toggleButton, checkedId, isChecked ->
+            if(isChecked){
+                when (checkedId){
+                    R.id.button1 -> {
+                        binding.cv5.visibility = View.GONE
+                        binding.cv4.visibility = View.INVISIBLE
+
+                        binding.button1.icon = AppCompatResources.getDrawable(applicationContext, R.drawable.baseline_check_24)
+                        binding.button2.icon = null
+                        binding.button3.icon = null
+                    }
+                    R.id.button2 -> {
+                        binding.cv5.visibility = View.GONE
+                        binding.cv4.visibility = View.VISIBLE
+
+                        binding.button1.icon = null
+                        binding.button2.icon = AppCompatResources.getDrawable(applicationContext, R.drawable.baseline_check_24)
+                        binding.button3.icon = null
+                    }
+                    R.id.button3 -> {
+                        binding.cv5.visibility = View.VISIBLE
+                        binding.cv4.visibility = View.VISIBLE
+
+                        binding.button1.icon = null
+                        binding.button2.icon = null
+                        binding.button3.icon = AppCompatResources.getDrawable(applicationContext, R.drawable.baseline_check_24)
+                    }
+                }
+            }
+        }
+        // 처음 키면 첫번째 옵션으로
+        binding.mbToggle.check(R.id.button1)
     }
 
     fun initCard(): MutableList<Card> {
@@ -42,4 +84,5 @@ class MainActivity : AppCompatActivity() {
         // 마지막 쉼표와 공백은 제외하고 콘솔창에 출력한다.
         Log.d("ShowCard", results.dropLast(2))
     }
+
 }
