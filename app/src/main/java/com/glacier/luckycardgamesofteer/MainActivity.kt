b4382fb62c456cba8b169fdef426c765c7dda223
Glacier Han <61905052+Glacier-Han@users.noreply.github.com>
2023-07-05 11:52:18 +0900

    FEAT: A영역 내카드 펼치기 기능 추가

--- app/src/main/java/com/glacier/luckycardgamesofteer/MainActivity.kt
+++ app/src/main/java/com/glacier/luckycardgamesofteer/MainActivity.kt
@@ -1,28 +1,43 @@
 package com.glacier.luckycardgamesofteer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.glacier.luckycardgamesofteer.adapter.CardAdapter
 import com.glacier.luckycardgamesofteer.databinding.ActivityMainBinding
-import kotlin.random.Random
+import com.glacier.luckycardgamesofteer.model.Animal
+import com.glacier.luckycardgamesofteer.model.Card
+import com.glacier.luckycardgamesofteer.model.Participant
+
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var cardList : MutableList<Card>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         init()
-        pickCard(initCard())
+
     }
 
     fun init(){
+        // https://selfish-developer.com/entry/%EA%B2%B9%EC%B9%98%EB%8A%94-recyclerview-%EB%A7%8C%EB%93%A4%EA%B8%B0
+
         binding.mbToggle.addOnButtonCheckedListener { toggleButton, checkedId, isChecked ->
             if(isChecked){
+                // 명수가 변경될 때 마다 카드를 새로 뽑는다
+                pickCard(initCard())
+
                 when (checkedId){
                     R.id.button1 -> {
                         binding.cv5.visibility = View.GONE
@@ -31,6 +46,38 @@ class MainActivity : AppCompatActivity() {
                         binding.button1.icon = AppCompatResources.getDrawable(applicationContext, R.drawable.baseline_check_24)
                         binding.button2.icon = null
                         binding.button3.icon = null
+
+                        // Share Cards
+                        // 12번 카드 3개 제외
+                        cardList.dropLast(3)
+
+                        // 카드 섞기
+                        cardList.shuffle()
+
+                        // 참가자들에게 카드 나눠주기
+                        val participant1 = Participant("1")
+                        participant1.addAllCard(cardList.take(8))
+
+                        val participant2 = Participant("2")
+                        participant2.addAllCard(cardList.take(8))
+
+                        val participant3 = Participant("3")
+                        participant3.addAllCard(cardList.take(8))
+
+                        val etcCards = Participant("0")
+                        etcCards.addAllCard(cardList.take(9))
+
+                        // recyclerview 어댑터 설정
+                        val cardAdapter = CardAdapter(participant1.getCards())
+                        val linearLayoutManager = LinearLayoutManager(applicationContext, RecyclerView.HORIZONTAL, false)
+
+                        binding.rv1.apply {
+                            layoutManager = linearLayoutManager
+                            adapter = cardAdapter
+                        }
+
+
+
                     }
                     R.id.button2 -> {
                         binding.cv5.visibility = View.GONE
@@ -57,12 +104,14 @@ class MainActivity : AppCompatActivity() {
 
     fun initCard(): MutableList<Card> {
         // 12개의 전체 카드 객체를 저장하기 위한 MutableList 선언
-        var cardList = mutableListOf<Card>()
+        cardList = mutableListOf()
 
         // AnimalInfo enum중 랜덤하게 뽑은 카드를 총 12개 생성 후 리스트에 담는다. 숫자는 1~12로 지정
-        for (i in 1..12) {
-            val pickedCard = Card(AnimalInfo.values().random(), i)
-            cardList.add(pickedCard)
+        for(i in 1..12){
+            for (animal in Animal.values()) {
+                val pickedCard = Card(animal, i)
+                cardList.add(pickedCard)
+            }
         }
 
         // 생성한 카드 리스트를 리턴한다.
@@ -74,7 +123,7 @@ class MainActivity : AppCompatActivity() {
         var results = ""
 
         // 뽑기 전에 카드들을 한번 섞는다
-        cardList.shuffle()
+        //cardList.shuffle()
 
         // 카드를 12개 다 뽑아서 콘솔에 정해진 형식대로 출력한다.
         for (card in cardList) {
@@ -83,6 +132,11 @@ class MainActivity : AppCompatActivity() {
 
         // 마지막 쉼표와 공백은 제외하고 콘솔창에 출력한다.
         Log.d("ShowCard", results.dropLast(2))
+    }
+
+    fun dp2px(ctx: Context, dp: Float): Int {
+        val scale = ctx.resources.displayMetrics.density
+        return (dp * scale + 0.5f).toInt()
     }
 
 }
---
+++ app/src/main/java/com/glacier/luckycardgamesofteer/model/Participant.kt
@@ -0,0 +1,33 @@
+package com.glacier.luckycardgamesofteer.model
+
+import android.util.Log
+
+class Participant(val name: String) {
+    private val cards: MutableList<Card> = mutableListOf()
+
+    fun addCard(card: Card) {
+        cards.add(card)
+    }
+
+    fun addAllCard(cardList: List<Card>){
+        cards.addAll(cardList)
+    }
+
+    fun removeCard(card: Card) {
+        cards.remove(card)
+    }
+
+    fun getCards(): List<Card> {
+        return cards
+    }
+
+    fun getCardCount(): Int{
+        return cards.size
+    }
+
+    fun showCards(){
+        for(c in cards){
+            Log.d("Participant $name", c.toString())
+        }
+    }
+}
