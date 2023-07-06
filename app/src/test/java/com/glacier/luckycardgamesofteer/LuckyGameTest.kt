400355a596678738cf72d5149c0ab6a988f38fd5
Glacier Han <61905052+Glacier-Han@users.noreply.github.com>
2023-07-06 17:55:50 +0900

    FEAT: JUnit 테스트 클래스 추가

---
+++ app/src/test/java/com/glacier/luckycardgamesofteer/LuckyGameTest.kt
@@ -0,0 +1,45 @@
+package com.glacier.luckycardgamesofteer
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class LuckyGameTest {
+
+    lateinit var luckyGame: LuckyGame
+
+    @Before
+    fun setUp() {
+        luckyGame = LuckyGame()
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun initCard() {
+    }
+
+    @Test
+    fun shareCard() {
+    }
+
+    @Test
+    fun sortCardAscend() {
+    }
+
+    @Test
+    fun sortRemainAscend() {
+    }
+
+    @Test
+    fun isSameCardInParticipants() {
+    }
+
+    @Test
+    fun isSameCardInSpecificCase() {
+    }
+}
