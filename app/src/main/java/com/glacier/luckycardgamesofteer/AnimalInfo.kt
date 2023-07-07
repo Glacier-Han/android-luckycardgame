4ea7303d785f0ba6680a0baaf36f7ff4c8c96e15
Glacier Han <61905052+Glacier-Han@users.noreply.github.com>
2023-07-07 15:08:08 +0900

    FIX: 필요없는 주석 제거 및 코딩 스타일 맞게 수정

--- app/src/main/java/com/glacier/luckycardgamesofteer/AnimalInfo.kt
+++ app/src/main/java/com/glacier/luckycardgamesofteer/AnimalInfo.kt
@@ -1,7 +1,7 @@
 package com.glacier.luckycardgamesofteer
 
 
-enum class AnimalInfo (val unicode: String) {
+enum class AnimalInfo(val unicode: String) {
     Dog("\uD83D\uDC36"),
     Cow("\uD83D\uDC31"),
     Cat("\uD83D\uDC2E"),
--- app/src/main/java/com/glacier/luckycardgamesofteer/MainActivity.kt
+++ app/src/main/java/com/glacier/luckycardgamesofteer/MainActivity.kt
@@ -14,32 +14,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun initCard(): MutableList<Card> {
-        // 12개의 전체 카드 객체를 저장하기 위한 MutableList 선언
         var cardList = mutableListOf<Card>()
 
-        // AnimalInfo enum중 랜덤하게 뽑은 카드를 총 12개 생성 후 리스트에 담는다. 숫자는 1~12로 지정
         for (i in 1..12) {
             val pickedCard = Card(AnimalInfo.values().random(), i)
             cardList.add(pickedCard)
         }
 
-        // 생성한 카드 리스트를 리턴한다.
         return cardList
     }
 
     fun pickCard(cardList: MutableList<Card>) {
-        // 콘솔에 결과를 표시하기 위한 String 변수
         var results = ""
 
-        // 뽑기 전에 카드들을 한번 섞는다
         cardList.shuffle()
 
-        // 카드를 12개 다 뽑아서 콘솔에 정해진 형식대로 출력한다.
         for (card in cardList) {
             results += String.format("%s%02d, ", card.animalType.unicode, card.num)
         }
 
-        // 마지막 쉼표와 공백은 제외하고 콘솔창에 출력한다.
+        // 마지막 쉼표와 공백은 제외하고 콘솔창에 출력하기 위해 dropLast(2)를 사용
         Log.d("ShowCard", results.dropLast(2))
     }
 }
